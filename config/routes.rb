3558807294328250d86261679910c04a5bbc449c
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-01-23 19:54:51 -0500

    adding rest functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  
   get "/first_contact", to: "contacts#first_contact"
-
   get "/contacts_form", to: "contacts#new_contact_method"
   post "/contacts_result", to: "contacts#new_contact_result_method"
   
+  get "/contacts", to: "contacts#index"
+  get "contacts/new", to: "contacts#new"
+  post "/contacts", to: "contacts#create"
+  get "/contacts/:id", to: "contacts#show"
+  get "/contacts/:id/edit", to: "contacts#edit"
+  patch "/contacts/:id", to: "contacts#update"
+  delete "/contacts/:id", to: "contacts#destroy"
 end
