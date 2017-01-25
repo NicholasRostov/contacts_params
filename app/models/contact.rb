945c94169da144973a4eecca927dd7c4b28180b7
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-01-24 20:21:40 -0500

    Adding model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+
+def time
+created_at.strftime("%b %d, %Y")
+  end
+def full_name
+ "#{first_name} #{last_name}"
+  end
+
+def jap_phone
+  "+81 #{phone_number}"
+  end
+
 end
