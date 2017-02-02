975b0c1fe3868cc1c0c6374b7a17465999c81d72
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-02-01 19:47:30 -0500

    Added sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "contacts#index"
   get "/first_contact", to: "contacts#first_contact"
   get "/contacts_form", to: "contacts#new_contact_method"
   post "/contacts_result", to: "contacts#new_contact_result_method"
@@ -10,4 +11,14 @@ Rails.application.routes.draw do
   get "/contacts/:id/edit", to: "contacts#edit"
   patch "/contacts/:id", to: "contacts#update"
   delete "/contacts/:id", to: "contacts#destroy"
+
+
+  # sign up
+  get "/signup", to:"users#new"
+  post "/users", to:"users#create"
+
+  # Sign In
+  get  "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 end
